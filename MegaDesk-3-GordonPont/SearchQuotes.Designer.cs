fdf2b381f047d87949c158a9eeef262e6caf2a8c
gpont1 <35281959+gpont1@users.noreply.github.com>
2019-04-01 07:42:23 -0600

    Fixed Final Stuff

--- MegaDesk-3-GordonPont/SearchQuotes.Designer.cs
+++ MegaDesk-3-GordonPont/SearchQuotes.Designer.cs
@@ -30,11 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.Exit = new System.Windows.Forms.Button();
+            this.listViewResults = new System.Windows.Forms.ListView();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.comboBoxMaterial = new System.Windows.Forms.ComboBox();
+            this.cancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(270, 372);
+            this.button1.Location = new System.Drawing.Point(250, 400);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 36);
             this.button1.TabIndex = 20;
@@ -44,7 +48,7 @@
             // 
             // Exit
             // 
-            this.Exit.Location = new System.Drawing.Point(393, 372);
+            this.Exit.Location = new System.Drawing.Point(373, 400);
             this.Exit.Name = "Exit";
             this.Exit.Size = new System.Drawing.Size(98, 36);
             this.Exit.TabIndex = 21;
@@ -52,16 +56,63 @@
             this.Exit.UseVisualStyleBackColor = true;
             this.Exit.Click += new System.EventHandler(this.Exit_Click);
             // 
+            // listViewResults
+            // 
+            this.listViewResults.Location = new System.Drawing.Point(44, 76);
+            this.listViewResults.Margin = new System.Windows.Forms.Padding(4);
+            this.listViewResults.Name = "listViewResults";
+            this.listViewResults.Size = new System.Drawing.Size(672, 350);
+            this.listViewResults.TabIndex = 25;
+            this.listViewResults.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.AutoSize = true;
+            this.buttonSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubmit.Location = new System.Drawing.Point(488, 9);
+            this.buttonSubmit.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(229, 41);
+            this.buttonSubmit.TabIndex = 24;
+            this.buttonSubmit.Text = "Search by Material";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxMaterial
+            // 
+            this.comboBoxMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxMaterial.FormattingEnabled = true;
+            this.comboBoxMaterial.Location = new System.Drawing.Point(44, 15);
+            this.comboBoxMaterial.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxMaterial.Name = "comboBoxMaterial";
+            this.comboBoxMaterial.Size = new System.Drawing.Size(347, 28);
+            this.comboBoxMaterial.TabIndex = 23;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(44, 457);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(4);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(139, 41);
+            this.cancelButton.TabIndex = 22;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
             // SearchQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(862, 603);
+            this.Controls.Add(this.listViewResults);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.comboBoxMaterial);
+            this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.Exit);
             this.Controls.Add(this.button1);
             this.Name = "SearchQuotes";
             this.Text = "SearchQuotes";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +120,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.ListView listViewResults;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.ComboBox comboBoxMaterial;
+        private System.Windows.Forms.Button cancelButton;
     }
 }
